c26abec3ffaea1c62e097081b659bbf589cfce74
Manikandan <manikandan@Manikandans-MacBook-Pro.local>
2017-05-07 00:09:27 +0530

    Login, registration, video popup

--- app/app.js
+++ app/app.js
@@ -19,6 +19,193 @@ var FirstSynch = angular.module("firstSync", [
 
 FirstSynch.constant('apiUrl', 'http://52.43.26.31:8000/');
 
+/////////////////////////////////////////////////Popup - Video, Login, Registration, Activate, Reset password, forgot password, logout///////////////
+//Video Popup Functionality
+FirstSynch.run(function($rootScope, $http, apiUrl) {
+    $rootScope.videoPopup = function (value) {
+      jQuery("#VideoPopup1").modal('show');
+      var id = value;
+      $http.get(apiUrl+"api/v1/flat_pages/rest/video_detail/"+id)
+      .then(function successCallback(response){
+          $rootScope.vid = response.data;
+      }, function errorCallback(response){
+          console.log("Unable to perform get Video Details");
+      });
+    };//Common Video Pupi - function end
+});
+
+
+//LoginUser
+FirstSynch.controller("Login", function ($scope, $http, apiUrl, $location, $window) {
+    $scope.LoginUser = function () {
+      var data = $.param({
+        username: $scope.username,
+        password: $scope.password,
+      });
+      $http({
+          url: apiUrl+'api/v1/accounts/login/',
+          method: "POST",
+          data: data,
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function successCallback(data, status, headers, config) {
+              $window.sessionStorage.setItem('token', data.token);
+              $location.path( "/student/dashboard" );
+      },function errorCallback(response){
+              $scope.status = 'Please provide valid login credentials.';
+      });
+
+    };// user login - function end
+
+});
+
+
+FirstSynch.controller("IdentifyUser", function ($scope, $http, apiUrl) {
+    $scope.SendData = function () {
+        var data = 'e_mail=' + $scope.e_mail;
+        $http({
+            url: apiUrl+'api/v1/accounts/signupemail/',
+            method: "POST",
+            data: data,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function successCallback(data, status, headers, config) {
+              if(data.user_type == '380D2'){
+                jQuery("#registration").modal('hide');
+                jQuery("#signUp").modal('show');
+                $("#email").val($scope.e_mail);
+              }else{
+                jQuery("#registration").modal('hide');
+                jQuery("#companyregistration").modal('show');
+                $("#email").val($scope.e_mail);
+              }
+        },function errorCallback(data, status, headers, config) {
+                $scope.error = data.data;
+        });
+
+    };//find user - function end
+
+    $scope.StudentRegistratoin = function () {
+
+      var education_created = {
+        school_name: $scope.school_name,
+        gpa: $scope.gpa,
+      };
+
+      var data = $.param({
+        e_mail: $scope.e_mail,
+        iaccept: $scope.iaccept,
+        name: $scope.name,
+        password: $scope.password,
+        education_created:education_created,
+      });
+
+      $http({
+            url: apiUrl+'api/v1/accounts/v2/createstudent/',
+            method: "POST",
+            data: data,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function successCallback(data, status, headers, config) {
+            jQuery("#signUp").modal('hide');
+            jQuery("#signUpSuccess").modal('show');
+      },function errorCallback(data, status, headers, config) {
+              $scope.status = status;
+      });
+
+    };//userStudentRegistratoin - function end
+
+    $scope.CompanyRegistratoin = function () {
+        var employee_created = {
+          company_name: $scope.cname,
+        };
+
+        var data = $.param({
+          e_mail: $scope.e_mail,
+          iaccept: $scope.iaccept,
+          name: $scope.name,
+          password: $scope.password,
+          employee_created:employee_created,
+        });
+        alert($scope.e_mail);
+        $http({
+            url: apiUrl+'api/v1/accounts/v2/createcompany/',
+            method: "POST",
+            data: data,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function successCallback(data, status, headers, config) {
+            jQuery("#companyregistration").modal('hide');
+            jQuery("#signUpSuccess").modal('show');
+        },function errorCallback(data, status, headers, config) {
+            $scope.status = status;
+        });
+
+      };//company Registratoin - function end
+
+});
+
+
+FirstSynch.controller("ForgotPassword", function ($scope, $http, apiUrl) {
+    $scope.ForgotPassword = function () {
+        var data = $.param({
+            e_mail: $scope.e_mail,
+        });
+        $http({
+              url: 'http://52.43.26.31:8000/api/v1/accounts/forgotpassword/',
+              method: "POST",
+              data: data,
+              headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+          }).then(function successCallback(data, status, headers, config) {
+              $scope.success = data.output;
+              $('.hide1').hide();
+          },function errorCallback(data, status, headers, config) {
+              $scope.failure = data.error;
+          });
+
+    };//Forgot Password - function end
+});
+
+
+FirstSynch.controller("ResetPassword", function ($scope, $http, apiUrl) {
+    $scope.ResetPassword = function () {
+        var data = $.param({
+            password: $scope.password,
+            token: $scope.token,
+        });
+        $http({
+              url: apiUrl+'api/v1/accounts/resetpasswordlink/',
+              method: "POST",
+              data: data,
+              headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function successCallback(data, status, headers, config) {
+              $scope.success = data.output;
+              $('.hide1').hide();
+              $scope.Login = $scope.Login + '<a data-toggle="modal" data-target="#logIn" href="#" data-dismiss="modal">Login</a>';
+        },function errorCallback(data, status, headers, config) {
+              $scope.failure = data.output;
+        });
+
+    };//Reset Password - function end
+});
+
+
+FirstSynch.controller("LogoutUser", function ($scope, $http, apiUrl, $window, $location) {
+    $scope.LogoutUser = function () {
+        var data = $.param({
+          token: $window.sessionStorage.getItem('token'),
+        });
+        $http({
+            url: apiUrl+'api/v1/accounts/logout/',
+            method: "POST",
+            data: data,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (data, status, headers, config) {
+                 $window.sessionStorage.removeItem('token');
+                 $window.location.href = '/';
+        }).error(function (data, status, headers, config) {
+                $scope.status = 'Please provide valid login credentials.';
+        });
+
+    };// user logout - function end
+});
+
 
 ///////////////////////////////////////////////// directive ////////////////////////////////////////
 FirstSynch.directive('uixBxslider', function(){
