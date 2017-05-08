504daff35ad4610fd5e316681aec053cf53cbca0
Manikandan <manikandan@Manikandans-MacBook-Pro.local>
2017-05-08 16:09:36 +0530

    activation popup added

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -23,14 +23,42 @@ $("#VideoPopup1").on("hidden.bs.modal", function () {
 		}
 });
 
-	$(window).on("load",function(){
+$( window ).on( "load", function() {
+    var getQueryString = function ( field, url ) {
+        var href = url ? url : window.location.href;
+        var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+        var string = reg.exec(href);
+        return string ? string[1] : null;
+    };
 
-				//$("#video-scroll-navi").mCustomScrollbar({
-					//setHeight:500,
-					//theme:"minimal-dark"
-				//});
+    var token = getQueryString('resetpassword');
+    if (token !== null && token !== undefined && token.length > 0 ) {
+            jQuery("#resetpassword").modal('show');
+            jQuery("#token").val(token);
+    }
 
-			});
+      //email activate function
+      var email_activate = getQueryString('activate');
+      if (email_activate !== null && email_activate !== undefined && email_activate.length > 0 ) {
+          jQuery("#activate").modal('show');
+          data = {'token': email_activate}
+          url = "http://52.43.26.31:8000/api/v1/accounts/activate/"
+
+          $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            success: function(output){
+              $('#activate_message').html('Congratulations! You have successfully registered!. Please activate your account.')
+            },
+            error: function(data){
+              $('#activate_message').html('Your Account is already active.')
+            }
+          });
+      }
+
+   });
+
 
 
 
