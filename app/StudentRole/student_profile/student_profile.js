faed8d27d9d7398a880d353fb1a29127707c1173
Admin <admin@firstsynch.com>
2017-05-08 06:18:08 +0530

    Student profile added to company,student and guest module

---
+++ app/StudentRole/student_profile/student_profile.js
@@ -0,0 +1,40 @@
+'use strict';
+/////////////////////////////////// Module ///////////////////////////////////////////
+
+var FirstSynch = angular.module("SstudentProfile", ["ngRoute"]);
+
+
+/////////////////////////////////// Configuration ////////////////////////////////////
+
+FirstSynch.config(['$locationProvider','$routeProvider', function($locationProvider,$routeProvider) {
+  $routeProvider.when('/student/student/profile', {
+    templateUrl: 'StudentRole/student_profile/student_profile.html',
+  });
+  if(window.history && window.history.pushState){
+     //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+     // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+     // if you don't wish to set base URL then use this
+    $locationProvider.html5Mode({
+       enabled: true,
+       requireBase: false
+    }).hashPrefix('!');
+   }
+}]);
+
+/////////////////////////////////// controllers ////////////////////////////////////
+
+// student details
+FirstSynch.controller("student_student_profile" ,function ($scope, $http,$routeParams,apiUrl) {
+
+  $http.get(apiUrl+"api/v1/user_profile/api/v1/student_profile/2/")
+      .then(function successCallback(response){
+          $scope.student_profile_details = response.data;
+          jwplayer("jwplayerforprofile").setup({
+            "file": response.data.video,
+            "primary": 'flash'
+          });
+      }, function errorCallback(response){
+          console.log("Unable to perform get student profile details");
+  });
+
+});
