a17aa230f2a1b0681f3f748d107e2e4a3f75832e
spmuthu21 <muthu@etekchnoservices.com>
2017-07-23 17:22:36 +0530

    rootscope issue fixed

--- app/GuestRole/career_fair/career_fair.js
+++ app/GuestRole/career_fair/career_fair.js
@@ -8,7 +8,7 @@ var FirstSynch = angular.module("careerFair", ["ngRoute"]);
 /////////////////////////////////// Controllors ////////////////////////////////////
 
 // career fair page - near by career fair
-FirstSynch.controller("guest_career_fair_near_user" ,function ($rootScope,$scope, $http,$routeParams,apiUrl,$rootScope) {
+FirstSynch.controller("guest_career_fair_near_user" ,function ($scope, $http,$routeParams,apiUrl,$rootScope) {
     $http.get(apiUrl+"api/v1/career_fairs/career_fair_near_current_user/?location="+$rootScope.current_city+"&count=10")
     .then(function successCallback(response){
         $scope.career_fair_near_current_user = response.data;
