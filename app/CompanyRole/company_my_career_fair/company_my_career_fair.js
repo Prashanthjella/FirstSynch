4a01c3b6412d5ac4300ef7cc22f062f9c04cdcda
admin <admin@firstsynch.com>
2017-08-14 02:34:08 +0530

    Career Fair request membership issues fixed - followed button issues fixed

--- app/CompanyRole/company_my_career_fair/company_my_career_fair.js
+++ app/CompanyRole/company_my_career_fair/company_my_career_fair.js
@@ -5,12 +5,13 @@ var FirstSynch = angular.module("CCompanyRequest", ["ngRoute"]);
 /////////////////////////////////// Controllors ////////////////////////////////////
 
 //Company My Career Fair page
-FirstSynch.controller("company_my_career_fair" ,function ($scope, $http, $routeParams, apiUrl, $rootScope) {
+FirstSynch.controller("company_my_career_fair" ,function ($scope, $http, apiUrl, $rootScope) {
 
-    $http.get(apiUrl+"api/v1/career_fairs/company_career_fair/"+$rootScope.companyedit_id+"/",{
+    $http.get(apiUrl+"api/v1/career_fairs/company_career_fair/"+$rootScope.company_userid+"/",{
           headers: {'Authorization' : 'Token '+$rootScope.token_id}
     }).then(function successCallback(response){
             $scope.my_career_fair = response.data;
+            $("#upcoming").css({"display": "block"});
     }, function errorCallback(response){
         console.log("Unable to perform get company career fair details");
     });
--- app/StudentRole/student_my_career_fair/student_my_career_fair.js
+++ app/StudentRole/student_my_career_fair/student_my_career_fair.js
@@ -5,12 +5,13 @@ var FirstSynch = angular.module("StudentFollowCareerFair", ["ngRoute"]);
 /////////////////////////////////// Controllors ////////////////////////////////////
 
 //Student My Career Fair page
-FirstSynch.controller("student_my_career_fair" ,function ($scope, $http, $routeParams, apiUrl, $rootScope) {
+FirstSynch.controller("student_my_career_fair" ,function ($scope, $http, apiUrl, $rootScope) {
 
     $http.get(apiUrl+"api/v1/career_fairs/student_career_fair/",{
           headers: {'Authorization' : 'Token '+$rootScope.token_id}
     }).then(function successCallback(response){
             $scope.my_career_fair = response.data;
+            $("#upcoming").css({"display": "block"});
     }, function errorCallback(response){
         console.log("Unable to perform get student career fair details");
     });
