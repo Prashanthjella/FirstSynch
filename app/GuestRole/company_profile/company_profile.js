f225065618a80175865337b894a24d9ffe230bc0
Prashanth Kumar <prashanth@firstsynch.com>
2017-11-06 18:11:21 -0700

    Updating retro style for maps

--- app/GuestRole/company_profile/company_profile.js
+++ app/GuestRole/company_profile/company_profile.js
@@ -8,21 +8,273 @@ var FirstSynch = angular.module("GcompanyProfile", ["ngRoute"]);
 
 // company details
 FirstSynch.controller("guest_company_profile" ,function ($rootScope,$timeout,$window,$scope, $http,$routeParams,apiUrl) {
-    $scope.guest_jobs_notfound = true;
-    $http.get(apiUrl+"api/v1/setups/"+$routeParams.companyid+"/",{
-        headers: {'Authorization' : 'Token '+$rootScope.token_id}
-    }).then(function successCallback(response){
-          $scope.company_profile_details = response.data;
-          $scope.guest_jobs_notfound = true;
-      }, function errorCallback(response){
-          console.log("Unable to perform get company profile details");
-          $scope.guest_jobs_notfound = false;
-    });
-  $scope.$watch('$viewContentLoaded', function(){
-      $timeout( function(){
-          $window.loading_screen.finish();
-     }, 3000 );
+  $scope.guest_jobs_notfound = true;
+  var map;
+  var lati;
+  var long;
+  var styledMapType = new google.maps.StyledMapType( // Have to create a JSON file and call it here.
+            [
+              {
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#ebe3cd"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#523735"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                  {
+                    "color": "#f5f1e6"
+                  }
+                ]
+              },
+              {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                  {
+                    "color": "#c9b2a6"
+                  }
+                ]
+              },
+              {
+                "featureType": "administrative.land_parcel",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                  {
+                    "color": "#dcd2be"
+                  }
+                ]
+              },
+              {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#ae9e90"
+                  }
+                ]
+              },
+              {
+                "featureType": "landscape.natural",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#dfd2ae"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#dfd2ae"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#93817c"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [
+                  {
+                    "color": "#a5b076"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#447530"
+                  }
+                ]
+              },
+              {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#f5f1e6"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#fdfcf8"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#f8c967"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                  {
+                    "color": "#e9bc62"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#e98d58"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                  {
+                    "color": "#db8555"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#806b63"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#dfd2ae"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.line",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#8f7d77"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.line",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                  {
+                    "color": "#ebe3cd"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#dfd2ae"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                  {
+                    "color": "#b9d3c2"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#92998d"
+                  }
+                ]
+              }
+            ],
+          {name: 'Styled Map'});
 
+  $http.get(apiUrl+"api/v1/setups/"+$routeParams.companyid+"/",{
+      headers: {'Authorization' : 'Token '+$rootScope.token_id}
+  }).then(function successCallback(response){
+      $scope.company_profile_details = response.data;
+      $scope.guest_jobs_notfound = true;
+      get_lat_long(response.data);
+    }, function errorCallback(response){
+      console.log("Unable to perform get company profile details");
+      $scope.guest_jobs_notfound = false;
+  });
+  $scope.$watch('$viewContentLoaded', function(){
+    $timeout( function(){
+      $window.loading_screen.finish();
+    }, 3000 );
   });
 
+  function get_lat_long(data) {
+    var geocoder = new google.maps.Geocoder();
+    var address =  data.contacts[0].address_1
+                +" "+data.contacts[0].city
+                +" "+data.contacts[0].country;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        lati = results[0].geometry.location.lat();
+        long = results[0].geometry.location.lng();
+        initMap()
+      } 
+    });
+  }
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: lati, lng: long},
+      zoom: 13,
+      mapTypeControlOptions: {
+        mapTypeIds: ['roadmap', 'satellite', 'styled_map']
+      }
+    });
+    
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+    var marker = new google.maps.Marker({
+     position: {lat: lati, lng: long},
+     map: map
+    });
+  }      
 });
